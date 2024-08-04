fe20f5fb3b1c6dcb44ab4a47bbdb2e2d5b9857db
nguyenthimynhi2409 <nguyenthimynhi2409@gmail.com>
2024-08-04 18:08:07 +0700

    test login

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,65 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export const validateInput = (input: string): boolean => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(input);
+};
+
+const App: React.FC = () => {
+    const handleSubmit = (e: React.SyntheticEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const form = e.target as HTMLFormElement;
+
+        const usernameInput = form.elements.namedItem(
+            'username'
+        ) as HTMLInputElement | null;
+        const passwordInput = form.elements.namedItem(
+            'password'
+        ) as HTMLInputElement | null;
+
+        if (usernameInput && passwordInput) {
+            const username = usernameInput.value;
+            const password = passwordInput.value;
+
+            if (username && password && validateInput(username)) {
+                alert('Success');
+            } else {
+                alert('Invalid input');
+            }
+        }
+    };
+
+    return (
+        <div className="login">
+            <div className="login__title">Login</div>
+            <form className="login_form" onSubmit={handleSubmit}>
+                <div className="login_form__input">
+                    <div className="login_form__item">
+                        <label htmlFor="username">Username</label>
+                        <input
+                            type="text"
+                            id="username"
+                            name="username"
+                            placeholder="Enter your username"
+                        />
+                    </div>
+                </div>
+                <div className="login_form__input">
+                    <div className="login_form__item">
+                        <label htmlFor="password">Password</label>
+                        <input
+                            type="password"
+                            id="password"
+                            name="password"
+                            placeholder="Enter your password"
+                        />
+                    </div>
+                </div>
+                <button className="login_form__submit">Submit</button>
+            </form>
+        </div>
+    );
+};
 
 export default App;
--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,9 +1,44 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from '../App';
+import React, { act } from 'react';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('login', () => {
+    beforeEach(() => {
+        window.alert = jest.fn(); // Mock alert before each test
+    });
+
+    test('should show success alert on valid form submission', () => {
+        render(<App />);
+        const usernameInput = screen.getByRole('textbox', {
+            name: /username/i,
+        });
+        const passwordInput = screen.getByLabelText('Password');
+        const submitButton = screen.getByRole('button', { name: /submit/i });
+
+        act(() => {
+            fireEvent.change(usernameInput, {
+                target: { value: 'text@test.com' },
+            });
+            fireEvent.change(passwordInput, { target: { value: 'password' } });
+            fireEvent.click(submitButton);
+        });
+        expect(window.alert).toHaveBeenCalledWith('Success');
+    });
+
+    test('should show invalid input alert on invalid form submission', () => {
+        render(<App />);
+        const usernameInput = screen.getByRole('textbox', {
+            name: /username/i,
+        });
+        const passwordInput = screen.getByLabelText('Password');
+        const submitButton = screen.getByRole('button', { name: /submit/i });
+        act(() => {
+            fireEvent.change(usernameInput, {
+                target: { value: 'invalid-email' },
+            });
+            fireEvent.change(passwordInput, { target: { value: 'password' } });
+            fireEvent.click(submitButton);
+        });
+        expect(window.alert).toHaveBeenCalledWith('Invalid input');
+    });
 });
